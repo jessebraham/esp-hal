c2979bd167191e20f12185db4351c14c69d28946
bjoernQ <bjoern.quentin@mobile-j.de>
2023-01-26 10:00:35 +0100

    Fix interrupt handling

--- esp-hal-common/build.rs
+++ esp-hal-common/build.rs
@@ -44,6 +44,7 @@ fn main() {
     //   - 'usb_otg'
     //   - 'usb_serial_jtag'
     //   - 'aes'
+    //   - 'plic'
     //
     // New symbols can be added as needed, but please be sure to update both this
     // comment and the required vectors below.
@@ -101,20 +102,7 @@ fn main() {
             "systimer",
             "timg1",
             "usb_serial_jtag",
-        ]
-    } else if esp32c6 {
-        vec![
-            "esp32c6",
-            "riscv",
-            "single_core",
-            "dac",
-            "gdma",
-            "i2s",
-            "mcpwm",
-            "rmt",
-            "systimer",
-            "timg1",
-            "usb_serial_jtag",
+            "plic",
         ]
     } else if esp32s2 {
         vec![
